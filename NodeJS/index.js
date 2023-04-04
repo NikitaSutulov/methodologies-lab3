b0ff521bff6ee6b741e05f7a81294ef7bcfe629a
Nikita Sutulov <notsid212@gmail.com>
2023-04-04 04:01:33 +0300

    added a simple nodejs project
    Added a simple nodejs project which writes the rainbow mnemonic painted in the rainbow colors in the console. In the future commit I will create a Dockerfile for it.

---
+++ NodeJS/index.js
@@ -0,0 +1,10 @@
+'use strict';
+const colors = require('colors');
+
+console.log(colors.red('Richard'));
+console.log(colors.yellow('Of'));
+console.log(colors.bgYellow('York'));
+console.log(colors.green('Gave'));
+console.log(colors.cyan('Battle'));
+console.log(colors.blue('In'));
+console.log(colors.magenta('Vain'));
